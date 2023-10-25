5cff49a1290b807fe13d60590a425cc9ed90da43
Prem Banker <bankerprem3@gmail.com>
2023-10-24 23:25:31 -0400

    151 q done of leetcode

--- 151.py
+++ 151.py
@@ -1,6 +1,16 @@
-class Solution {
-public:
-    string reverseWords(string s) {
-        
-    }
-};
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        ans = ''
+        word=''
+        for el in s.strip():
+
+            if el != ' ':
+                word+=el
+            else:
+                if word != '':
+                    ans= f' {word}' + ans
+                    word = ''
+
+
+        return word + ans
